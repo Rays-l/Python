28d2f621db088e6feefc5e591ec204bb29a19a90
root <root@localhost.localdomain>
2018-10-09 03:22:15 -0400

    4

---
+++ day04.py
@@ -0,0 +1,17 @@
+#_*_ -coding.utf-8
+class Rectangle:
+    def __init__ (self,wigth,height):
+        self.wigth=wigth
+        self.height=height
+    def getArea(self):
+        mianji = self.wigth*self.height
+        print mianji
+    def getPerimeter(self):
+        zhouchang = 2*self.wight+2*self,height
+        print zhouchang
+
+Rectangle(4,40).getArea()
+Rectangle(4,40).getPerimeter()
+
+
+
